02e99cc4d3f9e0b64083774e3d9b528d41fe6403
Natalia <napanova2007@gmail.com>
2024-12-04 19:17:27 +0200

    Initial commit

---
+++ quiz_data.py
@@ -0,0 +1,197 @@
+
+quiz_data = [
+    {
+        "question": "Вероника интересуется БАЛЕТОМ",
+        "choices": ["Именительный ","Дательный","Творительный","Родительный"],
+        "answer": "Творительный",
+        "correct": "'Творительный', потому что 'интересуется чем?'"
+
+    },
+    {
+        "question": "Вижу красивый ДОМ",
+        "choices": ["Винительный","Предложный","Дательный","Именительный"],
+        "answer": "Винительный",
+        "correct": "'Винительный', потому что 'виню что?'"
+    },
+
+]
+
+
+quiz_data2 = [
+    {
+        "name": "Vali õiget nimisõna vormi",
+        "question": "Nädalavahetustel me lähme (maa) ",
+        "choices": ["maasse","maal","maale","maas"],
+        "answer": "maale",
+        "correct": "'maale', sest see on alaleütlev kääne. Selles olukorras küsimusele 'kuhu?' vastame alaleütleva käändega."
+    },
+    {
+        "name": "Vali õiget nimisõna vormi",
+        "question": "Mari ja Jüri lähevad täna õhtul (teater)",
+        "choices": ["teatrisse","teatrist","teatrile","teatris"],
+        "answer": "teatrisse",
+        "correct": "'teatrisse', sest see on sisseütlev kääne. Selles küsimuses küsimusele 'kuhu?' vastame sisseütleva käändega. "
+    },
+    {
+        "name": "Vali õiget nimisõna vormi",
+        "question": "Ma istun (diivan) ",
+        "choices": ["diivanile","diivanil","diivanist","diivanisse"],
+        "answer": "diivanil",
+        "correct": "'diivanil', sest see on alalütlev kääne. Siin küsimusele 'kus?' vastame alalütleva käändega."
+    },
+{
+        "name": "Vali õiget nimisõna vormi",
+        "question": "Mina elan (Eesti)",
+        "choices": ["Eestil","Eestiga","Eesti","Eestis"],
+        "answer": "Eestis",
+        "correct": "'Eestis', sest see on seesütlev kääne. Siin küsimusele 'kus?' vastame seesütleva käändega."
+    },
+{
+        "name": "Vali õiget nimisõna vormi",
+        "question": "Ta pani raamatu (laud)",
+        "choices": ["lauale", "laual", "lauast","lauda"],
+        "answer": "lauale",
+        "correct": "'lauale', sest see on alaleütlev kääne. Siin küsimusele 'kuhu?' vastame alaleütleva käändega."
+    },
+{
+        "name": "Vali õiget nimisõna vormi",
+        "question": "Me jõudsime lõpuks (jõgi)",
+        "choices": ["jõest", "jõel", "jõgi", "jõeni"],
+        "answer": "jõeni",
+        "correct": "'jõeni', sest see on rajav kääne. Küsimusele 'milleni?' vastame rajava käändega."
+    },
+{
+        "name": "Vali õiget nimisõna vormi",
+        "question": "Ta lahkus kodust ilma (võti)",
+        "choices": ["võtmega", "võtmeid", "võtmeta", "võtme"],
+        "answer": "võtmeta",
+        "correct": "'võtmeta', sest see on ilmaütlev kääne, mis tähistab millegi puudumist. "
+    },
+{
+        "name": "Vali nimisõna vormi",
+        "question": "Me ostsime turult värskeid (õunad)",
+        "choices": ["õunu", "õunte", "õuntest", "õuntena"],
+        "answer": "õunu",
+        "correct": "'õunu', see on mitmuse osastava käändevorm, mis tähistab mingit hulka."
+    },
+{
+        "name": "Vali õiget nimisõna vormi",
+        "question": "Ma saatsin kutsed kõigile (külalised)",
+        "choices": ["külalistest", "külalistesse", "külalisteks", "külalistele" ],
+        "answer": "külalistele",
+        "correct": "'külalistele', sest see on mitmuse alaleütlev käändevorm ja esitav küsimus on 'kellele?'."
+    },
+{
+        "name": "Vali õiget nimisõna vormi",
+        "question": "Tema rääkis pikalt oma (probleemid)",
+        "choices": ["probleeme", "probleemidena","probleemidest", "probleemidega" ],
+        "answer": "probleemidest",
+        "correct": "'probleemidest', sest see on mitmuse seestütleva käändevorm ja küsimus on 'millest?'."
+    },
+    {
+        "name": "Vali õiget nimisõna käänet",
+        "question": "Me läheme (ujulasse)",
+        "choices": ["sisseütlev","seesütlev","seestütlev","rajav"],
+        "answer": "sisseütlev",
+        "correct": "'sisseütlev', sest nimisõna käändelõpp on '-sse'."
+    },
+{
+        "name": "Vali õiget nimisõna käänet",
+        "question": "Mis käändes on sõna 'majadeni?' ",
+        "choices": ["saav","rajav","alaleütlev", "omastav"],
+        "answer": "rajav",
+        "correct": "'rajav', sest nimisõna käändelõpp on '-ni'."
+    },
+{
+        "name": "Vali õiget nimisõna käänet",
+        "question": "Mis käändes on sõna 'koeraga'? ",
+        "choices": ["kaasaütlev", "saav", "olev", "rajav" ],
+        "answer": "kaasaütlev",
+        "correct": "'kaasaütlev', sest nimisõna käändelõpp on '-ga'."
+    },
+{
+        "name": "Vali õiget nimisõna käänet",
+        "question": "Mis käändes on sõna 'raamatut'?",
+        "choices": ["omastav", "nimetav", "osastav", "ilmaütlev"],
+        "answer": "osastav",
+        "correct": "'osastav', sest nimisõna käändelõpp on '-t'."
+    },
+{
+        "name": "Vali õiget nimisõna käänet",
+        "question": "Ma helistan (sõbrale)",
+        "choices": ["alalütlev", "alaleütlev", "sisseütlev", "kaasaütlev" ],
+        "answer": "alaleütlev",
+        "correct": "'alaleütlev', sest nimisõna käändelõpp on '-le'."
+    },
+{
+        "name": "Vali õiget nimisõna käänet",
+        "question": "Mis käändes on sõna 'toolid'?",
+        "choices": ["nimetav", "osastav", "omastav", "olev" ],
+        "answer": "nimetav",
+        "correct": "'nimetav', sest see on mitmuse nimetav käändevorm, mis moodustub ains. om. + d viisil. "
+    },
+{
+        "name": "Vali õiget nimisõna käänet",
+        "question": "Mis käändes on sõna 'õpilastena'?",
+        "choices": ["olev", "ilmaütlev", "saav", "kaasaütlev"],
+        "answer": "olev",
+        "correct": "'olev', sest nimisõna käändelõpp on '-na'."
+    },
+{
+        "name": "Vali õiget nimisõna käänet",
+        "question": "Me sõidame läbi (linnade)",
+        "choices": ["alaleütlev", "osastav", "rajav", "omastav" ],
+        "answer": "omastav",
+        "correct": "'omastav', sest see on nimisõna mitmuse omastava käändevorm, mis moodustub ains. os. + te/de viisil."
+    },
+{
+        "name": "Vali õiget nimisõna käänet",
+        "question": "Mis käändes on sõna 'lugudest'?",
+        "choices": ["saav", "sisseütlev", "seestütlev", "seesütlev"],
+        "answer": "seestütlev",
+        "correct": "'seestütlev', sest nimisõna käändelõpp on '-st'."
+    },
+{
+        "name": "Vali õiget nimisõna käänet",
+        "question": "Mis käändes on sõna 'muusikuteks'?",
+        "choices": ["nimetav" ,"saav", "alaleütlev", "olev"],
+        "answer": "saav",
+        "correct": "'saav', sest see on nimisõna mitmuse saava käändevorm, mis moodustub ains. om. + ks viisil."
+    },
+{
+        "name": "Vali õiget käändelõppu",
+        "question": "Mis käändelõpp on 'ilmaütlev' käändel ainsuses?",
+        "choices": ["-ta", "-ga", "-ks", "-lt" ],
+        "answer": "-ta",
+        "correct": "'-ta'"
+    },
+{
+        "name": "Vali õiget käändelõppu",
+        "question": "Mis käändelõpp on 'seestütlev' käändel ainsuses?",
+        "choices": ["-s", "-st", "-sse", "-ni" ],
+        "answer": "-st",
+        "correct": "'-st'"
+    },
+{
+        "name": "Vali õiget käändelõppu",
+        "question": "Mis käändelõpp on 'alaleülev' käändel ainsuses?",
+        "choices": ["-le", "-ga", "-l", "-lt"],
+        "answer": "-le",
+        "correct": "'-le'"
+    },
+{
+        "name": "Vali õiget käändelõppu",
+        "question": "Mis käändelõpp on 'kaasaütlev' käändel mitmuses?",
+        "choices": ["-tes/-des/-es", "-tega/-dega/-ega", "-teta/-deta/-eta", "-tel/-del/-el"],
+        "answer": "-tega/-dega/-ega",
+        "correct": "'-tega/-dega/-ega'"
+    },
+{
+        "name": "Vali õiget käändelõppu",
+        "question": "Mis käändelõpp on 'sisseütlev' käändel mitmuses?",
+        "choices": ["-tena/-dena/-ena", "-tele/-dele/-ele", "-tes/-des/-es", "-tesse/-desse/-esse"],
+        "answer": "-tesse/-desse/-esse",
+        "correct": "'-tesse/-desse/-esse'"
+    },
+
+]
